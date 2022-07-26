d2e03e6fda3712f7e1c6eafdb1f1cf7c152d36e7
Vedran Bilopavlović <vbilopav@gmail.com>
2022-07-26 09:57:32 +0200

    remoce scripts dir

--- Database/Scripts/Types/valid_genders.sql
+++
@@ -1,8 +0,0 @@
-DROP TYPE IF EXISTS public.valid_genders;
-
-CREATE TYPE public.valid_genders AS ENUM (
-    'M',
-    'F'
-);
-
-COMMENT ON TYPE public.valid_genders IS 'There are only two genders.';
